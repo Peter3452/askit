3a007b4fcaaec2ccc688a44555f60e5496acb1e0
Peter3452 <p1312402@gmail.com>
2022-09-04 13:01:29 +0300

    crud

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,7 +15,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
     if @question.save
-      redirect_to @question
+      redirect_to questions_path
     else
       render :new
     end
@@ -26,7 +26,7 @@ class QuestionsController < ApplicationController
 
   def update
     if @question.update(question_params)
-      redirect_to @question
+      redirect_to question_path
     else
       render :edit
     end
